1001a53059f2ab093f76f89b8020888532d7b1d8
Gorkaeff <gorka.jusue@gmail.com>
2016-01-13 11:33:16 +0100

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<div class="col-sm-offset-2 col-sm-8">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					Welcome
+				</div>
+
+				<div class="panel-body">
+					You can store tasks here! Please register or login.
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
