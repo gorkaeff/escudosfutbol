624c4fc032421641cf8db710bae071aa49d96f4c
Gorkaeff <gorka.jusue@gmail.com>
2016-01-15 11:03:59 +0100

    Add social and analyticstraking in differents blades

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,23 +10,16 @@
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	{!! HTML::style('/lib/bootstrap-table/bootstrap-table.min.css') !!}
 	{!! HTML::style('/css/escudos.css') !!}
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	{!! HTML::script('/lib/bootstrap-table/bootstrap-table.min.js') !!}
-	{!! HTML::script('/lib/bootstrap-table/bootstrap-table-es-ES.js') !!}
-
-	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.5";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+	@include('layouts.social')
 </head>
 
 <body>
 	@include('layouts.nav')
 	@yield('content')
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	{!! HTML::script('/lib/bootstrap-table/bootstrap-table.min.js') !!}
+	{!! HTML::script('/lib/bootstrap-table/bootstrap-table-es-ES.js') !!}
+	@include('layouts.analyticstracking')
 </body>
 </html>
