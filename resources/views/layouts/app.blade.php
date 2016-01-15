b938f6f92a001bb49dd176514d5ceaba96bdec83
Gorkaeff <gorka.jusue@gmail.com>
2016-01-15 18:19:57 +0100

    Footer include

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,10 @@
 
 <body>
 	@include('layouts.nav')
-	@yield('content')
+	<div class="contenidoPagina">
+		@yield('content')
+	</div>
+	@include('layouts.footer')
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	{!! HTML::script('/lib/bootstrap-table/bootstrap-table.min.js') !!}
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-7">
+				<p class="text-muted">
+					Desarrollado por: <a href="http://gorkaeff.github.io/">Gorkaeff</a>
+				</p>
+			</div>
+			<div class="col-md-5">
+				<a href="https://github.com/gorkaeff/escudosfutbol">Código fuente del proyecto en Github</a>
+			</div>
+		</div>
+	</div>
+</footer>
